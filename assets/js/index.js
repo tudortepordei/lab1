e6eb4372b259353d8f4ee11ace087d209eaacd0c
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-02-26 02:32:47 +0200

    Iaca și butoanele

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Definim ordinea paginilor
+    
     const pages = ["index.html", "geography.html", "landmarks.html", "cuisine.html", "history.html"];
 
-    // Obținem numele paginii curente
     const currentPage = window.location.pathname.split("/").pop();
 
-    // Găsim indexul paginii curente în listă
     let currentIndex = pages.indexOf(currentPage);
 
-    // Butoane
     const prevButton = document.querySelector(".prev-page");
     const nextButton = document.querySelector(".next-page");
 
-    // Navigare la pagina anterioară
     prevButton.addEventListener("click", function () {
-        if (currentIndex > 0) {
-            window.location.href = pages[currentIndex - 1];
-        } else {
-            alert("Aceasta este prima pagină!");
-        }
+            window.location.href = pages[4];
     });
 
-    // Navigare la pagina următoare
     nextButton.addEventListener("click", function () {
-        if (currentIndex < pages.length - 1) {
-            window.location.href = pages[currentIndex + 1];
-        } else {
-            alert("Aceasta este ultima pagină!");
-        }
-    });
+        window.location.href = pages[currentIndex + 1]
+    })
+        
 });
